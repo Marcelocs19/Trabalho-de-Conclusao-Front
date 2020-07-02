31246f1fd1c78c4bc8537ec84719886ff5d83432
Marcelo <m_campos_19@hotmail.com>
2020-07-02 10:00:18 -0300

    ✨ Mudado o mapa para o open street view e melhorias no cadastro, no editar e na criação da rota

--- src/pages/cadastro-aluno/cadastro-aluno.ts
+++ src/pages/cadastro-aluno/cadastro-aluno.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { AlunosProvider } from '../../providers/alunos/alunos';
+declare var google;
 
 @IonicPage()
 @Component({
@@ -17,14 +18,26 @@ export class CadastroAlunoPage {
   sobrenome: string;
   cpf: string;
 
+  //autocomplete
+  autoCompleteItems = [];
+  GoogleAutocomplete;
+  address: string = '';
+  selecionouEnd: boolean = false;
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
+    public loader: LoadingController,
     public viewCtrl: ViewController,
     private alunosService: AlunosProvider) {
+      this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+
   }
 
   cadastrar() {
+    let loader = this.loader.create({
+      content: 'Cadastrando...'
+    });
+    loader.present();
     let params = { 
       nome: this.nome,
       sobrenome: this.sobrenome, 
@@ -32,21 +45,58 @@ export class CadastroAlunoPage {
       bairro: this.bairro,
       numero: this.numero,
       email: this.email,
-      cpf: this.cpf
+      cpf: this.cpf,
+      enderecoCompleto: this.address
     }
     this.alunosService.cadastrar(params).subscribe(retorno => {
-      console.log(retorno);
+      loader.dismiss();
       if (retorno) {
         this.viewCtrl.dismiss(retorno);
-        
       } else {
         this.viewCtrl.dismiss()
       }
     }, err => {
+      loader.dismiss();
+
       console.log('erro: ', err);
     })
     
 
+  }
+
+  updateSearchResults() {
+    //busca a lista de enderecos de acordos com o que o usuario digita
+    if (!this.endereco) {
+      this.autoCompleteItems = [];
+      this.selecionouEnd = false;
+      return;
+    }
+    if (this.endereco == this.address) {
+      return this.autoCompleteItems = [];
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.endereco, componentRestrictions: { country: 'br' } },
+      (predictions, status) => {
+        this.autoCompleteItems = [];
+        if (predictions) {
+          predictions.forEach((prediction) => {
+            if (prediction.description == this.endereco) return this.autoCompleteItems = [];
+            this.autoCompleteItems.push(prediction);
+          });
+        } else {
+          this.autoCompleteItems = [];
+        }
+      })
+  }
+
+  selectAddress(address) {
+    console.log(address)
+    // ao selecionar um end. da lista, usa ele para passar por parametro e coloca na model do end.
+    this.address = address.description;
+    this.autoCompleteItems = [];
+    this.selecionouEnd = true;
+    this.endereco = address.terms[0].value;
+    this.numero = address.terms[1].value;
+    this.bairro = address.terms[2].value;
   }
 
 }
--- src/pipes/filter.pipe.ts
+++ src/pipes/filter.pipe.ts
@@ -7,21 +7,28 @@ import { Events } from 'ionic-angular';
 export class FilterBuscaPipe implements PipeTransform {
 
   propriedadesBusca = [
-    'nome', 'sobrenome', 'endereco', 'numero', 'cpf'
+    'nome', 'sobrenome', 'endereco', 'numero', 'cpf', 'bairro'
   ];
 
   constructor(private event: Events) { }
 
   transform(alunos, filter): any {
       console.log(alunos);
-    return alunos.filter(item => {
-      return (
-        item.nome.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
-        item.sobrenome.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
-        item.endereco.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
-        item.numero.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
-        item.cpf.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1
-      );
-    });
+      if (alunos) {
+        console.log('a')
+        return alunos.filter(item => {
+          return (
+            item.nome.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+            item.sobrenome.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+            item.endereco.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+            item.numero.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+            item.cpf.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+            item.bairro.toLowerCase().indexOf(filter.toLowerCase()) !== -1 
+          );
+        });
+      } else {
+        console.log('b')
+      }
+    
   }
 }
